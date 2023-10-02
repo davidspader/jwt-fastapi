3dcd9fb6696477cb272855c3f873812f930dc743
David Ambrozio <davidambroziodev@gmail.com>
2023-10-02 16:05:31 -0300

    create user login

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, status
 from fastapi.responses import JSONResponse
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from app.depends import get_db_session
 from app.auth_user import UserUseCases
@@ -10,11 +11,28 @@ router = APIRouter(prefix='/user')
 @router.post('/register')
 def user_register(
     user: User,
-    db_session: Session = Depends(get_db_session)
+    db_session: Session = Depends(get_db_session),
 ):
     uc = UserUseCases(db_session=db_session)
     uc.user_register(user=user)
     return JSONResponse(
         content={'msg': 'sucesses'},
         status_code=status.HTTP_201_CREATED
+    )
+
+@router.post('/login')
+def user_login(
+    request_form_user: OAuth2PasswordRequestForm = Depends(),
+    db_session: Session = Depends(get_db_session),
+):
+    uc = UserUseCases(db_session=db_session)
+    user = User(
+        username=request_form_user.username,
+        password=request_form_user.password
+    )
+
+    auth_data = uc.user_login(user=user)
+    return JSONResponse(
+        content=auth_data,
+        status_code=status.HTTP_200_OK
     )
